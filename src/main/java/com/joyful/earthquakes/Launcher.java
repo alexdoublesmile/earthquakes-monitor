1af94b66bcb431fc56c4e5372a51b097a15bce8a
Александр Плохой <alexplohoy@mail.ru>
2023-02-07 07:07:51 +0300

    add jsoup

--- src/main/java/com/joyful/earthquakes/Launcher.java
+++ src/main/java/com/joyful/earthquakes/Launcher.java
@@ -1,7 +1,13 @@
 package com.joyful.earthquakes;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Launcher {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws MalformedURLException {
+//        final URLConnection conn = new URL("https://earthquake.usgs.gov/earthquakes/map/?currentFeatureId=us6000jm5j&extent=6.05316,-16.43555&extent=64.39694,88.94531").openConnection();
+
+
     }
 }
