28993bd5dddd8e22e0e4b339e1fefab4bf4e416d
Александр Плохой <alexplohoy@mail.ru>
2023-02-08 15:23:49 +0300

    fix(controller): add find by location endpoint

--- src/main/java/com/joyful/earthquakes/controller/EarthEventController.java
+++ src/main/java/com/joyful/earthquakes/controller/EarthEventController.java
@@ -4,17 +4,22 @@ import com.joyful.earthquakes.model.dto.EarthEventReadDto;
 import com.joyful.earthquakes.service.EarthEventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Log4j2
 @RestController
+@RequestMapping("/api/v1/earth-events")
 @RequiredArgsConstructor
 public class EarthEventController {
     private EarthEventService earthEventService;
 
-    public List<EarthEventReadDto> findByLocationName(String location) {
+    @GetMapping
+    public List<EarthEventReadDto> findByLocationName(@RequestParam String location) {
         return earthEventService.findByLocationName(location);
     }
 }
